5aa234e1c18489093c480b0598051d4efd1f104f
aattifhuossiane-art <aattifhuossiane@gmail.com>
2025-12-06 23:47:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-// index.js - Codice che ignora la chiave
 
-module.exports = async (req, res) => {
-    // Il server si avvia senza cercare la chiave segreta
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    
-    // Messaggio che conferma l'avvio, ma non l'uso dell'AI
-    res.end('Ponte Segreto Attivo. NESSUNA CHIAVE AI NECESSARIA PER L\'AVVIO.');
-
-    // *** QUI AGGIUNGEREMO LA LOGICA AI E LA CHIAVE DOPO ***
-};
 
